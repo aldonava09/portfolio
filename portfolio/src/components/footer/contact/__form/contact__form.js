c349ab6c4250a04c7dabf349621325c182423f22
aldonava09 <aldonavargas@gmail.com>
2023-07-22 01:14:00 -0600

    Agregar media-queries 1024px

--- portfolio/src/components/footer/contact/__form/contact__form.js
+++ portfolio/src/components/footer/contact/__form/contact__form.js
@@ -1,20 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../../styles/footer/contact/__form/contact__form.css';
 
 function ContactForm() {
-    return (
-      <div className='contact__form-container'>
-        <form className='contact__form' noValidate>
-        <input className='contact__form-name-input' type="text" id="nombre" name="nombre" placeholder='NAME' required></input>
-        <span class="contact__form-name-error">Sorry,invalid format here!</span>
-        <input className='contact__form-email-input' type="email" id="email" name="email" placeholder='EMAIL' required></input>
-        <span class="contact__form-email-error">Sorry,invalid format here!</span>
-        <textarea className='contact__form-message-input' id="message" name="message" rows="4" placeholder='MESSAGE' required></textarea>
-        <span class="contact__form-message-error">Sorry,invalid format here!</span>
-        <button className='contact__form-button' type="submit">SEND MESSAGE</button>
-        </form>
-      </div>
-    );
+  const validationObj = {
+    name: "",
+    email: "",
+    message: ""
+  };
+
+  const [form, setForm] = useState(validationObj);
+  const [focus, setFocus] = useState({ name: false, email: false, message: false });
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  function handleFormInput(e) {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
   }
-  
-  export default ContactForm;
+
+  const [nameValid, setNameValid] = useState(null);
+  const [emailValid, setEmailValid] = useState(null);
+  const [messageValid, setMessageValid] = useState(null);
+
+  useEffect(() => {
+    const regExp = {
+      name: /^(?:[a-zA-Z]+|)$/,
+      email: /^(?:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|)$/,
+      message: /^(?:[\s\S]{3,150}|)$/,
+    };
+
+    if (focus.name || form.name === "") {
+      setNameValid(form.name === "" ? false : regExp.name.test(form.name));
+    }
+
+    if (focus.email || form.email === "") {
+      setEmailValid(form.email === "" ? false : regExp.email.test(form.email));
+    }
+
+    if (focus.message || form.message === "") {
+      setMessageValid(form.message === "" ? false : regExp.message.test(form.message));
+    }
+
+    setIsFormValid(form.name !== "" && form.email !== "" && form.message !== "" && nameValid && emailValid && messageValid);
+  }, [form, focus, nameValid, emailValid, messageValid]);
+
+  function handleFocus(field) {
+    setFocus((prevFocus) => ({
+      ...prevFocus,
+      [field]: true,
+    }));
+  }
+
+  return (
+    <div className='contact__form-container'>
+      <form className='contact__form' noValidate>
+        <h3 className='contact__form-title'>CONTACT FORM</h3>
+        <input
+          className={`contact__form-name-input ${focus.name && (nameValid ? 'contact__form-name-input_active' : 'contact__form-name-input_error')}`}
+          type="text"  id="nombre"  name="name" placeholder='NAME' value={form.name} onChange={handleFormInput} onFocus={() => handleFocus('name')} required/>
+        {focus.name && form.name === "" && <span className="contact__form-name-error">This field is required!</span>}
+        {focus.name && nameValid === false && form.name !== "" && <span className="contact__form-name-error">Sorry, invalid format, special characters not available!</span>}
+        <input
+          className={`contact__form-email-input ${focus.email && (emailValid ? 'contact__form-email-input_active' : 'contact__form-email-input_error')}`}
+          type="email" id="email" name="email" placeholder='EMAIL' value={form.email} onChange={handleFormInput} onFocus={() => handleFocus('email')} required/>
+        {focus.email && form.email === "" && <span className="contact__form-email-error">This field is required!</span>}
+        {focus.email && emailValid === false && form.email !== "" && <span className="contact__form-email-error">Sorry, invalid format here, add email!</span>}
+        <textarea
+          className={`contact__form-message-input ${focus.message && (messageValid ? 'contact__form-message-input_active' : 'contact__form-message-input_error')}`}
+          id="message" name="message" rows="4" placeholder='MESSAGE' value={form.message} onChange={handleFormInput} onFocus={() => handleFocus('message')} required></textarea>
+        {focus.message && form.message === "" && <span className="contact__form-message-error">This field is required!</span>}
+        {focus.message && messageValid === false && form.message !== "" && <span className="contact__form-message-error">Sorry, invalid format here, add more characters!</span>}
+        <button className={`contact__form-button ${!isFormValid ? 'contact__form-button_disabled' : ''}`} type="submit" disabled={!isFormValid}>SEND MESSAGE</button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
