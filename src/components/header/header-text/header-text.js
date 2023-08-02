d9a5b31fac01a67634e97f291ed885fbfbb79de5
aldonava09 <aldonavargas@gmail.com>
2023-08-02 00:05:36 -0600

    Correccion de bug Header

--- src/components/header/header-text/header-text.js
+++ src/components/header/header-text/header-text.js
@@ -5,7 +5,7 @@ function HeaderText({contactLink}) {
     return (
       <div className="header-text__container">
         <h1 className='header-text__title'>Nice to meet you! I’m <span className='header-text__title-name'>Aldo Navarro</span>.</h1>
-        <p className='header-text__paragraph'>Based in the México, I’m a front-end developer passionate about building accessible web apps that users love.</p>
+        <p className='header-text__paragraph'>Based in México, I’m a front-end developer passionate about building accessible web apps that users love.</p>
         <a className='header-text__link' href={contactLink}>CONTACT ME</a>
       </div>
     );
