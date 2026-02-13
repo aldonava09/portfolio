06f9be696b28aac825d0c7124a077b3e5ae9ee01
aldonava09 <aldonavargas@gmail.com>
2026-02-13 13:22:49 -0600

    Portafolio revisado

--- src/components/main/projects/projects.js
+++ src/components/main/projects/projects.js
@@ -2,60 +2,54 @@ import React from 'react';
 import Card from './__card/projects__card';
 import '../../../styles/main/projects/projects.css';
 import Rings from '../../../images/pattern-rings.svg';
-import artPageImg from '../../../images/proyecto-pagina-galeria-arte.png';
-import socialMediaImg from '../../../images/proyecto-red-social.png';
-import agencyPageImg from '../../../images/proyecto-pagina-agencia.png';
-import salesCardPageImg from '../../../images/proyecto-tarjetas-venta.png';
-import landingPageBlogImg from '../../../images/proyecto-pagina-blog.png';
+import retailImg from '../../../images/retail-dashboard.png';
+import revenueDriversImg from '../../../images/revenue-drivers-dashboard.png';
+import lendingPortfolioImg from '../../../images/lending-portfolio-dashboard.png';
+
 
 const projectsSet = [
-    {
-      image: artPageImg,
-      name: 'ART GALERY PAGE',
-      technologies: 'HTML-CSS',
-      projectLink: 'https://aldonava09.github.io/Pagina-de-patria-a-patria/',
-      codeLink: 'https://github.com/aldonava09/Pagina-de-patria-a-patria',
-      projectDescription:'"De patria a patria" project is a one-page website, which functions as the contact for a photo gallerying. It has been designed with the objective of practicing the use of HTML, CSS and responsive design, making sure that all elements display correctly on the most common screen sizes.'
-    },
-    {
-      image: socialMediaImg,
-      name: 'SOCIAL MEDIA PROYECT',
-      technologies: 'HTML-CSS-JAVASCRIPT-WEBPACK-APIs-REST',
-      projectLink: 'https://aldonava09.github.io/Project-Social-Network/',
-      codeLink: 'https://github.com/aldonava09/Project-Social-Network',
-      projectDescription: 'This project is a social network that has been developed using HTML, CSS, and JavaScript. REST APIs have been used to interact with the server, and the BEM methodology has been applied. project also utilizes the Webpack bundler to manage dependencies and optimize the code.'
-    },
-    {
-      image: agencyPageImg,
-      name: 'WEB DESIGNER AGENCY',
-      technologies: 'HTML-CSS-JAVASCRIPT-BOOSTRAP',
-      projectLink: 'https://aldonava09.github.io/Proyecto-Boostrap/',
-      codeLink: 'https://github.com/aldonava09/Proyecto-Boostrap',
-      projectDescription: 'This project is a web page for a web development agency specialized in creating sites for other businesses, using CSS, HTML JavaScript, by the use of Bootstrap framework.'
-    },
-    {
-      image: salesCardPageImg,
-      name: 'SALES CARDS LANDING PAGE',
-      technologies: 'HTML-CSS-REACTJS-VITE',
-      projectLink: 'https://aldonava09.github.io/project-sales-card/',
-      codeLink: 'https://github.com/aldonava09/project-sales-card',
-      projectDescription: 'This project is a React-based landing page designed and developed to promote a subscription for a cloud storage service. The page showcases sales cards with detailed information about various available plans.'
-    },
-    {
-      image: landingPageBlogImg,
-      name: 'BLOG LANDING PAGE',
-      technologies: 'HTML-CSS',
-      projectLink: 'https://aldonava09.github.io/Proyecto-Blog-Landing-Page/',
-      codeLink: 'https://github.com/aldonava09/Proyecto-Blog-Landing-Page',
-      projectDescription: 'The project is a one-page website, it is designed to expose the information in a visually attractive way, since it has a series of images, figures, third-party video resources and responsive links which they reinforce the impact of the information by being interactive with the user.'
-    }
-]
+  {
+    image: retailImg,
+    name: 'Retail Sales Performance Analysis',
+    technologies: 'Excel | KPI Development | Sales Analysis',
+    projectLink: 'https://github.com/aldonava09/portfolio-data-analyst/tree/main/01-excel',
+    codeLink: 'https://github.com/aldonava09/portfolio-data-analyst',
+    projectDescription:
+      'Sales performance analysis focused on identifying revenue trends, category performance, and key business KPIs. Developed structured Excel dashboards to support data-driven commercial decisions.'
+  },
+  {
+    image: revenueDriversImg,
+    name: 'Product Revenue Drivers Analysis',
+    technologies: 'SQL | Power BI | Data Validation | KPI Modeling',
+    projectLink: 'https://github.com/aldonava09/portfolio-data-analyst/tree/main/02-sql-powerbi',
+    codeLink: 'https://github.com/aldonava09/portfolio-data-analyst',
+    projectDescription:
+      'Business-oriented SQL analysis to identify whether revenue is driven by pricing or sales volume. Includes data validation, metric modeling, and executive dashboard visualization in Power BI.'
+  },
+  {
+    image: lendingPortfolioImg,
+    name: 'Lending Portfolio Risk & Profitability Analysis',
+    technologies: 'Python | PostgreSQL | SQL | Power BI | Data Modeling',
+    projectLink: 'https://github.com/aldonava09/portfolio-data-analyst/tree/main/03-sql-python-powerbi',
+    codeLink: 'https://github.com/aldonava09/portfolio-data-analyst',
+    projectDescription:
+      'End-to-end financial portfolio analysis using Lending Club data. Included ETL with Python, relational modeling in PostgreSQL (3NF), risk and ROI evaluation, and executive dashboard development.'
+  }
+];
+
 
 function Projects({ data }) {
     return (
         <section className="projects">
-            <h2 className='projects__title'>Projects</h2>
-            <a className='projects__link' href={'#footer'}>CONTACT ME</a>
+            <h2 className='projects__title'>Data Projects</h2>
+            <a 
+  className='projects__link' 
+  href="https://github.com/aldonava09" 
+  target="_blank" 
+  rel="noopener noreferrer"
+>
+  VIEW ALL PROJECTS
+</a>
             <img className='projects__rings_top' src={Rings} alt='Rings Decoration'/>
             <div className='projects__cards-container'>
                 {data.map((item, index) => (
--- src/components/main/skills/__card/skills__card.js
+++ src/components/main/skills/__card/skills__card.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-function SkillsCard({ logo, name, experience, isBlack}) {
-    return (
-      <div className="skills__card">
-        <img className={`skills__card-logo ${isBlack ? 'skills__card-logo_white' : ''}`} src={logo} alt={name + " Logo"} />
-        <div className="skills__card-text">
-          <h3 className="skills__card-header">{name}</h3>
-          <p className="skills__card-paragraph">{experience}</p>
-        </div>
-      </div>
-    );
+function SkillsCard({ name, level, url }) {
+  return (
+    <div className="skills__item">
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="skills__link"
+      >
+        {name}
+      </a>
+
+      <p className="skills__level">{level}</p>
+    </div>
+  );
 }
 
 export default SkillsCard;
